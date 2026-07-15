fb195c267e3de6b3cd94dbd9cb8e5a289b240d04
Asilbek Norbekov <asilbeknorbekov@users.noreply.github.com>
2026-07-15 22:59:01 +0500

    Fix: remove share_plus Kotlin conflict, fix app.dart import paths

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'presentation/routing/app_router.dart';
-import 'core/design_system/glass_theme.dart';
+import 'routing/app_router.dart';
+import '../core/design_system/glass_theme.dart';
 
 final sharedPrefsProvider = Provider<SharedPreferences>((ref) => throw UnimplementedError());
 
