ad2321818ac5a096fa0389610541e31ee2946731
Asilbek Norbekov <asilbeknorbekov@users.noreply.github.com>
2026-07-16 19:07:29 +0500

    fix: import EncryptionService in security_provider.dart

--- lib/core/security/security_provider.dart
+++ lib/core/security/security_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'security_service.dart';
+import 'encryption_service.dart';
 
 final securityServiceProvider = Provider<SecurityService>((ref) {
   return SecurityService();
